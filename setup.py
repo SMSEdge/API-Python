a49eb30b5df4e2188817dfc2f3da1a0e757def39
Mimon Copitman <mimon@smsedge.io>
2019-10-29 13:06:46 +0200

    - Change setup.py config

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="smsedge_api_python-SMSEdge", # Replace with your own username
-    version="1.1.0",
+    version="1.4.0",
     author="Mimon Copitman",
     author_email="mimon@smsedge.io",
     description="SMSEdge API package for Python development",
-    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/SMSEdge/API-Python",
     packages=setuptools.find_packages(),
